c64b0dce61e408405b21694de624be48785e66fb
nSedrickm <nsedrick101@gmail.com>
2020-07-03 22:34:30 +0100

    D2

--- Express_MySQL_CRUD/index.js
+++ Express_MySQL_CRUD/index.js
@@ -7,40 +7,40 @@ const mysql = require('mysql');
 const app = express();
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'raider',
-    password: 'raider_dev',
-    database: 'inventory_development'
+  host: 'localhost',
+  user: 'raider',
+  password: 'raider_dev',
+  database: '__DEKU__'
 });
 
 connection.connect((err) => {
-    if(err) {
-        throw(err);
-    }
-    console.log('Connected')
+  if (err) {
+    throw (err);
+  }
+  console.log('Connected')
 });
 
-app.set('views',path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //route for homepage
-app.get('/',(req, res) => {
-    let sql = "SELECT * FROM Products";
-    let query = connection.query(sql, (err, data) => {
-      if(err) {
-        res.render('error');
-        throw err;
-      }
+app.get('/', (req, res) => {
+  let sql = "SELECT * FROM MODEM_SMS_RECEIVED";
+  let query = connection.query(sql, (err, data) => {
+    if (err) {
+      res.render('error');
+      throw err;
+    }
 
-      res.render('index',{
-        data: data
-      });
+    res.render('index', {
+      data: data
     });
   });
+});
 
-  app.listen(8001, () => {
-    console.log('Server is running at port 8001');
-  });
+app.listen(8001, () => {
+  console.log('Server is running at port 8001');
+});
