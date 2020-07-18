63eafb12e1e6506463e06f0a6eedac991668cbb2
nsedrickm <nsedrick101@gmail.com>
2020-07-18 11:13:35 +0100

    st

--- mockup/js/main.js
+++ mockup/js/main.js
@@ -1,11 +1,14 @@
-var next_button = document.getElementByID("next_button");
-next_button.addEventListener("click", getNextQuestion());
+$("#next_button_one").on("click", function (e) {
+  $("#question_1").toggleClass("d-none");
+  $("#question_2").toggleClass("d-none");
+});
 
-var question_title = document.getElementByID("question-title");
+$("#next_button_two").on("click", function (e) {
+  $("#question_2").toggleClass("d-none");
+  $("#question_3").toggleClass("d-none");
+});
 
-var question = document.getElementByID("question");
-
-function getNextQuestion() {
-  question_title.textContent = "2 of 70";
-  question.textContent = "2) this is question 2";
-}
+$("#next_button_three").on("click", function (e) {
+  $("#question_3").toggleClass("d-none");
+  $("#question_4").toggleClass("d-none");
+});
